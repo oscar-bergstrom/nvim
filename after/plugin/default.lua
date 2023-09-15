09e84f5dfe41c8649d7cfd54d4b64f390ecc1a71
Oscar Bergstrom <oscar.bergstrom@scandinovasystems.com>
2023-09-15 14:50:21 +0200

    Changed tabstop to 4 spaces and highlight column 79

--- after/plugin/default.lua
+++ after/plugin/default.lua
@@ -2,3 +2,8 @@ vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>su', require('telescope.builtin').lsp_references, { desc = '[S]earch [Usages]' })
+
+vim.opt.tabstop = 4
+vim.opt.colorcolumn = "79"
+
+
