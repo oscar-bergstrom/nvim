230a0d0b73b9c69fb45439484ebec0f3831b7d11
Oscar Bergstrom <oscar.bergstrom@scandinovasystems.com>
2023-09-27 08:31:15 +0200

    Added keymaps for entering terminal or to invoke custom run script on current file

--- after/plugin/default.lua
+++ after/plugin/default.lua
@@ -6,4 +6,21 @@ vim.keymap.set('n', '<leader>su', require('telescope.builtin').lsp_references, {
 vim.opt.tabstop = 4
 vim.opt.colorcolumn = "79"
 
+-- Terminal settings 
 
+local on_run = function()
+		local fname = vim.api.nvim_buf_get_name(0)
+		vim.api.nvim_command('tabnew | term ./run.sh ' .. fname)
+end
+
+local t_opts = {silent = true}
+
+local keymap = vim.keymap.set
+keymap('t', '<esc>',     '<C-\\><C-N>', t_opts)
+keymap('t', '<C-Left>',  '<C-\\><C-N><C-w>h', t_opts)
+keymap('t', '<C-Down>',  '<C-\\><C-N><C-w>j', t_opts)
+keymap('t', '<C-Up>',    '<C-\\><C-N><C-w>k', t_opts)
+keymap('t', '<C-Right>', '<C-\\><C-N><C-w>l', t_opts)
+
+keymap('n', '<leader>rt', '<cmd>tabnew | term<cr>', {desc = '[R]un [T]erminal'})
+keymap('n', '<leader>rf', on_run, {desc = '[R]un [F]ile'})
