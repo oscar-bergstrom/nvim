fde4a9b54b5c01f5fccead180c887e068c3030fe
Oscar Bergstrom <oscar.bergstrom@scandinovasystems.com>
2024-04-08 13:41:21 +0200

    Added option to Run Previous file, regardless of which file that is currently in use

--- after/plugin/default.lua
+++ after/plugin/default.lua
@@ -9,9 +9,15 @@ vim.opt.colorcolumn = "79"
 
 -- Terminal settings 
 
+local prev_fname = ""
+
 local on_run = function()
-	local fname = vim.api.nvim_buf_get_name(0)
-	vim.api.nvim_command('tabnew | term ./run.sh ' .. fname)
+	prev_fname = vim.api.nvim_buf_get_name(0)
+	vim.api.nvim_command('tabnew | term ./run.sh ' .. prev_fname)
+end
+
+local on_run_prev = function()
+	vim.api.nvim_command('tabnew | term ./run.sh ' .. prev_fname)
 end
 
 local t_opts = {silent = true}
@@ -25,3 +31,4 @@ keymap('t', '<C-Right>', '<C-\\><C-N><C-w>l', t_opts)
 
 keymap('n', '<leader>rt', '<cmd>tabnew | term<cr> | i', {desc = '[R]un [T]erminal'})
 keymap('n', '<leader>rf', on_run, {desc = '[R]un [F]ile'})
+keymap('n', '<leader>rp', on_run_prev, {desc = '[R]un [P]revious file'})
